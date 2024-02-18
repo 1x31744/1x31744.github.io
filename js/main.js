e5ebd5087473fc485375d25bcc94e6dc3d458f3d
Koopa <koopahello@gmail.com>
2024-02-18 21:00:08 +0000

    starting on files window

--- js/main.js
+++ js/main.js
@@ -3,13 +3,27 @@ let terminal_read_part = document.getElementById("terminalReadspace")
 let edit_content = document.getElementById("termedit")
 let about_content = document.getElementById("about_content")
 
+const files = new WinBox("Files", {
+    background: "#FF0000",
+    border: 4,
+    width: '500px',
+    height: '800px',
+    x: "10%",
+    y: "center",
+    top: "72px",
+    mount: document.getElementById("files_content"),
+})
 const terminal = new WinBox("Terminal",{
     background: "#FF0000",
     border: 4,
     width: '800px',
-    height: '400px',
+    height: '500px',
+    x: "center",
+    y: "center",
+    top: "72px",
     mount: document.getElementById("term_content"),
 });
+edit_content.focus()
 
 edit_content.addEventListener('keydown', (e) => {
     if (e.key == "Enter") {
@@ -18,6 +32,7 @@ edit_content.addEventListener('keydown', (e) => {
         terminal_read_part.textContent += "kxxpa@DESKTOP-ANJNVHI:~$ " + term_command
         switch (term_command) {
             case "help":
+                terminal_read_part.textContent += "\n\n  about : learn about me and what i enjoy doing\n  social : see the social media i have\n  projects : see a list of the projects i've made\n  clear : clear the terminal\n  title : display the title in terminal\n  contact : all the ways you can contact me\n  help : you just used this\n "
                 break;
             case "clear":
                 terminal_read_part.textContent = " "
@@ -25,6 +40,7 @@ edit_content.addEventListener('keydown', (e) => {
             case "title":
                 terminal_read_part.textContent = "\n"
                 terminal_read_part.textContent += " ██ ▄█▀▒██   ██▒▒██   ██▒▄▄▄█████▓▓█████  ██▀███   ███▄ ▄███▓\n ██▄█▒ ▒▒ █ █ ▒░▒▒ █ █ ▒░▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒▓██▒▀█▀ ██▒\n▓███▄░ ░░  █   ░░░  █   ░▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒▓██    ▓██░\n▓██ █▄  ░ █ █ ▒  ░ █ █ ▒ ░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ▒██    ▒██\n▒██▒ █▄▒██▒ ▒██▒▒██▒ ▒██▒  ▒██▒ ░ ░▒████▒░██▓ ▒██▒▒██▒   ░██▒\n▒ ▒▒ ▓▒▒▒ ░ ░▓ ░▒▒ ░ ░▓ ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░   ░  ░\n░ ░▒ ▒░░░   ░▒ ░░░   ░▒ ░    ░     ░ ░  ░  ░▒ ░ ▒░░  ░      ░\n░ ░░ ░  ░    ░   ░    ░    ░         ░     ░░   ░ ░      ░   \n░  ░    ░    ░   ░    ░              ░  ░   ░            ░\n\nWelcome to my portfolio!\nType help to see commands to learn about me!\nOr, you can use the buttons in the tab bar to open windows.\n "
+                break;
             case "about":
                 const aboutBox = new WinBox({
                     title: 'About Me',

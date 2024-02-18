fd10c76c9169182adf6837a81ad6a00d4cce10e9
Koopa <koopahello@gmail.com>
2024-02-18 17:02:50 +0000

    started on making content boxes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 /* make terminal via non editable div and editable div */
 let terminal_read_part = document.getElementById("terminalReadspace")
 let edit_content = document.getElementById("termedit")
+let about_content = document.getElementById("about_content")
 
 const terminal = new WinBox("Terminal",{
     background: "#FF0000",
     border: 4,
     width: '800px',
     height: '400px',
-    mount: document.getElementById("content"),
+    mount: document.getElementById("term_content"),
 });
 
 edit_content.addEventListener('keydown', (e) => {
@@ -25,6 +26,14 @@ edit_content.addEventListener('keydown', (e) => {
                 terminal_read_part.textContent = "\n"
                 terminal_read_part.textContent += " ██ ▄█▀▒██   ██▒▒██   ██▒▄▄▄█████▓▓█████  ██▀███   ███▄ ▄███▓\n ██▄█▒ ▒▒ █ █ ▒░▒▒ █ █ ▒░▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒▓██▒▀█▀ ██▒\n▓███▄░ ░░  █   ░░░  █   ░▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒▓██    ▓██░\n▓██ █▄  ░ █ █ ▒  ░ █ █ ▒ ░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ▒██    ▒██\n▒██▒ █▄▒██▒ ▒██▒▒██▒ ▒██▒  ▒██▒ ░ ░▒████▒░██▓ ▒██▒▒██▒   ░██▒\n▒ ▒▒ ▓▒▒▒ ░ ░▓ ░▒▒ ░ ░▓ ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░   ░  ░\n░ ░▒ ▒░░░   ░▒ ░░░   ░▒ ░    ░     ░ ░  ░  ░▒ ░ ▒░░  ░      ░\n░ ░░ ░  ░    ░   ░    ░    ░         ░     ░░   ░ ░      ░   \n░  ░    ░    ░   ░    ░              ░  ░   ░            ░\n\nWelcome to my portfolio!\nType help to see commands to learn about me!\nOr, you can use the buttons in the tab bar to open windows.\n "
             case "about":
+                const aboutBox = new WinBox({
+                    title: 'About Me',
+                    background: "#FF0000",
+                    border: 4,
+                    height: '400px',
+                    width: '400px',
+                    mount: document.getElementById("about_content").cloneNode(true),
+                })
                 break;
             case "who":
                 break;

2b8d8fc08f4f62f0f3fa1dea1d31710394a149f8
Koopa <koopahello@gmail.com>
2024-02-20 22:19:32 +0000

    finished main page (pretty sure, other than extra content in future)

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ edit_content.addEventListener('keydown', (e) => {
         terminal_read_part.textContent += "kxxpa@DESKTOP-ANJNVHI:~$ " + term_command
         switch (term_command) {
             case "help":
-                terminal_read_part.textContent += "\n\n  about : learn about me and what i enjoy doing\n  social : see the social media i have\n  projects : see a list of the projects i've made\n  clear : clear the terminal\n  title : display the title in terminal\n  contact : all the ways you can contact me\n  help : you just used this\n "
+                terminal_read_part.textContent += "\n\n  about : learn about me and what i enjoy doing\n  social : see the social media i have\n  projects : see a list commands to see the projects i've made\n  clear : clear the terminal\n  title : display the title in terminal\n  contact : all the ways you can contact me\n  help : you just used this\n "
                 break;
             case "clear":
                 terminal_read_part.textContent = " "
@@ -51,7 +51,47 @@ edit_content.addEventListener('keydown', (e) => {
                     mount: document.getElementById("about_content").cloneNode(true),
                 })
                 break;
-            case "who":
+            case "contact":
+                const contactBox = new WinBox({
+                    title: 'Contact',
+                    background: "#FF0000",
+                    border: 4,
+                    height: '320px',
+                    width: '400px',
+                    mount: document.getElementById("contact_content").cloneNode(true),
+                })
+            case "projects":
+                terminal_read_part.textContent += "\n\n(remember to type the command for more info)\n\n  raytracecpu : a cpu based ray tracer that uses common ray tracing (and some path tracing) techniques\n\n  chip8 : a chip 8 emulator that i made to learn about emulators, i plan to make more ;)\n\n  steel : an abstract syntax tree interpreted language that i made to learn about how interpreters work, i also plan to make some more\n\n"
+                break;
+            case "raytracecpu":
+                const rayProjBox = new WinBox({
+                    title: 'Ray Tracer',
+                    background: "#FF0000",
+                    border: 4,
+                    height: '600px',
+                    width: '800px',
+                    mount: document.getElementById("ray_proj_content").cloneNode(true),
+                });
+                break;
+            case "chip8":
+                const chip8ProjBox = new WinBox({
+                    title: 'Chip 8 Emulator',
+                    background: "#FF0000",
+                    border: 4,
+                    height: '450px',
+                    width: '800px',
+                    mount: document.getElementById("chip8_content").cloneNode(true),
+                });
+                break;
+            case "steel":
+                const steelProjBox = new WinBox({
+                    title: 'Steel Interpreter',
+                    background: "#FF0000",
+                    border: 4,
+                    height: '450px',
+                    width: '800px',
+                    mount: document.getElementById("steel_proj_content").cloneNode(true),
+                });
                 break;
             default:
                 terminal_read_part.textContent += "\nUnrecognized command: '" + term_command+ "', type 'help' to see all available commands."
@@ -103,6 +143,18 @@ about_file.addEventListener('click', () => {
     })
 });
 
+let contact_file = document.getElementById("contact_button")
+contact_file.addEventListener('click', () => {
+    const contactBox = new WinBox({
+        title: 'Contact',
+        background: "#FF0000",
+        border: 4,
+        height: '320px',
+        width: '400px',
+        mount: document.getElementById("contact_content").cloneNode(true),
+    })
+});
+
 let ray_proj_button = document.getElementById("ray_proj_button")
 ray_proj_button.addEventListener('click', () => {
     const rayProjBox = new WinBox({
@@ -113,4 +165,28 @@ ray_proj_button.addEventListener('click', () => {
         width: '800px',
         mount: document.getElementById("ray_proj_content").cloneNode(true),
     })
+});
+
+let steel_proj_button = document.getElementById("steel_proj_button")
+steel_proj_button.addEventListener('click', () => {
+    const steelProjBox = new WinBox({
+        title: 'Steel Interpreter',
+        background: "#FF0000",
+        border: 4,
+        height: '450px',
+        width: '800px',
+        mount: document.getElementById("steel_proj_content").cloneNode(true),
+    });
+});
+
+let chip8_proj_button = document.getElementById("chip8_proj_button")
+chip8_proj_button.addEventListener('click', () => {
+    const chip8ProjBox = new WinBox({
+        title: 'Chip 8 Emulator',
+        background: "#FF0000",
+        border: 4,
+        height: '450px',
+        width: '800px',
+        mount: document.getElementById("chip8_content").cloneNode(true),
+    });
 });

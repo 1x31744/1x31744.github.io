abf0adeea6f99a547b542a9d22a3dfd0a36aad14
Koopa <koopahello@gmail.com>
2024-02-24 15:48:18 +0000

    further done blog page

---
+++ js/blog.js
@@ -0,0 +1,20 @@
+// do this but with a div instead
+
+new WinBox("Posts", {
+    top: "10%",
+    right: "70%",
+    bottom: "0%",
+    minheight: "100%",
+    minwidth: "100%",
+    border: 4,
+    background: "#FF0000"
+});
+new WinBox("Viewer", {
+    top: "10%",
+    border: 4,
+    left: "30%",
+    bottom: "0%",
+    minheight: "100%",
+    minwidth: "100%",
+    background: "#FF0000"
+});

d8b2a7481b5e8301c6902aa2477a0954cf2a3403
kxxpa <koopahello@gmail.com>
2024-05-28 15:49:18 +0100

    updated mobile version for blog to account for past changes

--- js/mobile_blog.js
+++ js/mobile_blog.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('pre code').forEach((block) => {
       hljs.highlightElement(block);
     });
-  });
+});
+
+
 
 let dropdown_content = document.getElementById("blog_selector");
 let dropdown_button = document.getElementById("dropdown");
@@ -45,6 +47,59 @@ os_folder.addEventListener('click', () => {
     }
 });
 
+let nes_folder = document.getElementById('nes_file');
+let nes_column = document.getElementById('nes_column');
+let nes_visible = true
+nes_folder.addEventListener('click', () => {
+    nes_visible = !nes_visible
+    if (nes_visible) {
+        nes_column.style.display = 'none'
+    }
+    else {
+        nes_column.style.display = 'block'
+    }
+})
+
+let readability_checkbox = document.getElementById("read_check")
+let docs = document.getElementsByClassName("doc")
+docs = Array.from(docs)
+docs.push(blog_content)
+console.log(typeof(docs))
+readability_checkbox.addEventListener('change', e => {
+    if(e.target.checked === true) {
+        for (var i = 0; i < docs.length; i++) {
+            let doc = docs[i]
+            let children = doc.querySelectorAll('*')
+            for (var j = 0; j < children.length; j++) {
+                if (children[j].tagName == "P" || children[j].tagName == "H1" ||
+                    children[j].tagName == "H2" || children[j].tagName == "H3" ||
+                    children[j].tagName == "UL"
+                ){
+                    children[j].style.color = 'white'
+                }
+            }
+        }
+
+    }
+    if (e.target.checked === false) {
+        for (var i = 0; i < docs.length; i++) {
+            let doc = docs[i]
+            let children = doc.querySelectorAll('*')
+            for (var j = 0; j < children.length; j++) {
+                if (children[j].tagName == "P" || children[j].tagName == "H1" ||
+                    children[j].tagName == "H2" || children[j].tagName == "H3" ||
+                    children[j].tagName == "UL"
+                ){
+                    children[j].style.color = 'red'
+                }
+            }
+        }
+    }
+})
+window.onload = function() {
+    readability_checkbox.checked = false;
+};
+
 //file buttons
 let about_file = document.getElementById("about_button")
 let about_content = document.getElementById("about_content")
@@ -76,4 +131,29 @@ os2_file.addEventListener('click', () => {
     dropdown_content.style.display = 'none'
     root_visible = !root_visible
 })
+let os3_file = document.getElementById("os_part3_button")
+let os3_content = document.getElementById("os3_content")
+os3_file.addEventListener('click', () => {
+    blog_content.innerHTML = os3_content.innerHTML
+    dropdown_content.style.display = 'none'
+    root_visible = !root_visible
+})
+
+//nesbuttons
+let nes1_file = document.getElementById("nes_part1_button")
+let nes1_content = document.getElementById("nes1_content")
+nes1_file.addEventListener('click', () => {
+    blog_content.innerHTML = nes1_content.innerHTML
+    dropdown_content.style.display = 'none'
+    root_visible = !root_visible
+})
+
+let nes2_file = document.getElementById("nes_part2_button")
+let nes2_content = document.getElementById("nes2_content")
+nes2_file.addEventListener('click', () => {
+    blog_content.innerHTML = nes2_content.innerHTML
+    dropdown_content.style.display = 'none'
+    root_visible = !root_visible
+})
+
 
